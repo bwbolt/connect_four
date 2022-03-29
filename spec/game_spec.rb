2c08e298bd18d0a310b4f602666e3f7a03c953a8
Bryce Wein <bryce.wein@gmail.com>
2022-03-29 14:43:21 -0400

    Test: Add game initializes with a board test.

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -13,4 +13,15 @@ describe Game do
     expect(game.player1).to be_a(Player)
     expect(game.player2).to be_a(Player)
   end
+  it 'initializes with a board' do
+    game = Game.new
+    expect(game.board).to be_a(Board)
+    expect(game.board.board).to eq({ 'a' => '.,.,.,.,.,.',
+                                     'b' => '.,.,.,.,.,.',
+                                     'c' => '.,.,.,.,.,.',
+                                     'd' => '.,.,.,.,.,.',
+                                     'e' => '.,.,.,.,.,.',
+                                     'f' => '.,.,.,.,.,.',
+                                     'g' => '.,.,.,.,.,.' })
+  end
 end
