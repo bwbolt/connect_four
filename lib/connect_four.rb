9d2e16c1953500dfbb73625331908ab4e9ac5256
Saba Bhamidipati <saba.bhamidipati@gmail.com>
2022-03-29 13:12:27 -0400

    feat: Create welcome message and code for user response

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,2 +1,14 @@
+require './lib/game'
+
 p "Welcome to CONNECT FOUR"
 p "Enter p to play. Enter q to quit."
+
+def user_response
+  p "Enter p to play. Enter q to quit."
+    user_input = gets.chomp
+  if user_input == "p"
+    game.start
+  elsif user_input == "q"
+    p "Hope to see you again soon! Goodbye!"
+  end
+end
