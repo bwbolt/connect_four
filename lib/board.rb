51840449d01d0a9bfefae0ad2050ebd4d58cb170
Saba Bhamidipati <saba.bhamidipati@gmail.com>
2022-03-29 18:34:56 -0400

    feat: Create functioning board and test board-spec tests

--- lib/board.rb
+++ lib/board.rb
@@ -1,10 +1,8 @@
 class Board
+attr_reader :board
 
   def initialize
-  end
-
-  def board
-    field = {
+    @board = {
       'a' => '.,.,.,.,.,.',
       'b' => '.,.,.,.,.,.',
       'c' => '.,.,.,.,.,.',
@@ -13,27 +11,24 @@ class Board
       'f' => '.,.,.,.,.,.',
       'g' => '.,.,.,.,.,.'
     }
-    field.each do |key, value|
-      make_string = "#{value}".gsub(',', ' ')
-      puts make_string
-    end
   end
 
-  # def drawBoard
-  #       board = Hash.new
-  #       (1..6).each do |i|
-  #       ('a'..'g').each do |letter|
-  #               board["#{i}#{letter}"] = "*"
-  #               print board["#{i}#{letter}"]
-  #           end
-  #           puts
-  #       end
-  # end
+  def print_board
+  a = [5, 4, 3, 2, 1, 0]
+
+  puts 'ABCDEFG'
+  a.each do |i|
+    puts "#{@board['a'].split(',')[i]}#{@board['b'].split(',')[i]}#{@board['c'].split(',')[i]}#{@board['d'].split(',')[i]}#{@board['e'].split(',')[i]}#{@board['f'].split(',')[i]}#{@board['g'].split(',')[i]}"
+  end
+  end
 end
 
-board = Board.new
-board.board
 
-# p board.drawBoard
-# board = Board.new
-# p board.board
+
+board = Board.new
+
+board.print_board
+
+
+
+  
