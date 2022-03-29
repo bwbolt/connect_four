b641276b9654acd3d0021f8bf343954e21749959
Saba Bhamidipati <saba.bhamidipati@gmail.com>
2022-03-29 16:11:31 -0400

    feat: Create board class and develop connect four board that prints

--- lib/board.rb
+++ lib/board.rb
@@ -4,7 +4,37 @@ class Board
   end
 
   def board
-    
+    field = {
+      'a' => '.,.,.,.,.,.',
+      'b' => '.,.,.,.,.,.',
+      'c' => '.,.,.,.,.,.',
+      'd' => '.,.,.,.,.,.',
+      'e' => '.,.,.,.,.,.',
+      'f' => '.,.,.,.,.,.',
+      'g' => '.,.,.,.,.,.'
+    }
+    field.each do |key, value|
+      make_string = "#{key}#{value}"
+      make_string.split(",")
+      print make_string
+    end
   end
 
+  # def drawBoard
+  #       board = Hash.new
+  #       (1..6).each do |i|
+  #       ('a'..'g').each do |letter|
+  #               board["#{i}#{letter}"] = "*"
+  #               print board["#{i}#{letter}"]
+  #           end
+  #           puts
+  #       end
+  # end
 end
+
+board = Board.new
+p board.board
+
+# p board.drawBoard
+# board = Board.new
+# p board.board
