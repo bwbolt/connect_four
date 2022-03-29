f8e9c9fc48ac07264d1328f9268206b88361d7ef
Bryce Wein <bryce.wein@gmail.com>
2022-03-29 11:27:34 -0400

    Test: Add test of board class having a board and test of printing board.

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -6,4 +6,21 @@ describe Board do
     board = Board.new
     expect(board).to be_a Board
   end
+  it 'has columns and rows' do
+    board = Board.new
+    expect(board.board).to eq({
+                                'a' => '.,.,.,.,.,.',
+                                'b' => '.,.,.,.,.,.',
+                                'c' => '.,.,.,.,.,.',
+                                'd' => '.,.,.,.,.,.',
+                                'e' => '.,.,.,.,.,.',
+                                'f' => '.,.,.,.,.,.',
+                                'g' => '.,.,.,.,.,.'
+
+                              })
+  end
+  it 'can print board' do
+    board = Board.new
+    expect(board).to respond_to(:print_board)
+  end
 end
