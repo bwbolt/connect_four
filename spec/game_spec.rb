dcfc0a0ce7fa0ab2b026119a210cd44028bed1c1
Bryce Wein <bryce.wein@gmail.com>
2022-03-29 14:40:04 -0400

    Test: Add game initializes with players test.

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,9 +1,16 @@
 require 'rspec'
+require './lib/player'
+require './lib/board'
 require './lib/game'
 
 describe Game do
   it 'exists' do
     game = Game.new
     expect(game).to be_a Game
+  end
+  it 'initializes with a player1 and a player2' do
+    game = Game.new
+    expect(game.player1).to be_a(Player)
+    expect(game.player2).to be_a(Player)
   end
 end
