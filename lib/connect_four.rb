f5430ea15052c6a79d69a08195be895ce455f0b8
Bryce Wein <bryce.wein@gmail.com>
2022-03-31 15:00:30 -0400

    Refactor: Use print main menu method instead of hard coded

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,13 +1,5 @@
 require './lib/game'
 
+game = Game.new
 p 'Welcome to CONNECT FOUR'
-p 'Enter p to play. Enter q to quit.'
-
-user_input = nil
-user_input = gets.chomp until %w[p q].include?(user_input)
-if user_input == 'p'
-  game = Game.new
-  game.start
-elsif user_input == 'q'
-  p 'Hope to see you again soon! Goodbye!'
-end
+game.print_main_menu
