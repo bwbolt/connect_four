184062183dd890c173b50c163ecd2ff14fc7e3f0
Bryce Wein <bryce.wein@gmail.com>
2022-04-03 00:04:29 -0400

    Refactor: Refactor method length

--- lib/board.rb
+++ lib/board.rb
@@ -14,11 +14,11 @@ class Board
   end
 
   def print_board
-    a = [5, 4, 3, 2, 1, 0]
-
     puts 'ABCDEFG'
-    a.each do |i|
-      puts "#{@board['a'].split(',')[i]}#{@board['b'].split(',')[i]}#{@board['c'].split(',')[i]}#{@board['d'].split(',')[i]}#{@board['e'].split(',')[i]}#{@board['f'].split(',')[i]}#{@board['g'].split(',')[i]}"
+    [5, 4, 3, 2, 1, 0].each do |i|
+      row = ''
+      %w[a b c d e f g].each { |column| row << @board[column].split(',')[i] }
+      puts row
     end
   end
 
